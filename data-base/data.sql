211ee79daac24fb3fd5c0b625c3138754777857a
admin <>
2022-10-13 16:04:27 +0200

    modif + query

--- data-base/data.sql
+++ data-base/data.sql
@@ -1,17 +1,17 @@
---tableau message--
-CREATE TABLE IF NOT EXISTS message (
-    id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    id_du_jeu varchar(10) NOT NULL,
-    id_du_joueur varchar(10) NOT NULL,
-    msg text NOT NULL,
-    date_heure datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-    PRIMARY KEY (id)
+--tab msg--
+CREATE TABLE IF NOT EXISTS `msg` (
+    `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id_du_jeu` int(10) NOT NULL,
+    `id_du_joueur` int(10) NOT NULL,
+    `msg` text NOT NULL,
+    `date_heure` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
 
---tableau jeu--
-CREATE TABLE IF NOT EXISTS jeu (
-    id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    nom_jeu varchar(40) NOT NULL,
-    PRIMARY KEY (id)
+--tab game--
+CREATE TABLE IF NOT EXISTS `jeu` (
+    `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `nom_jeu` varchar(40) NOT NULL,
+    PRIMARY KEY (`id`)
 )
 
