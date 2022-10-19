0721be63484af73efe5f2ff7222d2c9a1730ad11
Kenza Tighrine <k.tghrn@outlook.fr>
2022-10-19 19:22:49 +0200

    tab_score + scores + select

--- tableaux_des_scores/scores.php
+++ tableaux_des_scores/scores.php
@@ -1,22 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="tab_score.css">
+<?php
+  require '../include/og_header.inc.php';
+  ?>
+    <link rel="stylesheet" href="../tab_score/tab_score.css">
     <title>Tableaux des scores</title>
 </head>
 <body>
 
-    <section class="showcase">
-        <div class="video-container">
-    <video autoplay="autoplay" muted="" loop="infinite" src="../tab_score/video.mp4"></video> 
-    </div>
-    <div class="contacterh1">
-        <strong><h1>TABLEAUX DES SCORES</h1></strong>
-    </section>
-
+<?php
+  require '../include/header.inc.php';
+  ?>
     <div class="score1">
 
         <div class ="reussiteclicks">
@@ -32,7 +27,7 @@
                 <p id="scoreréussite2">50%</p>
 
              </div>
-                
+
              <div class = "clicks">
                 <h1 class="titrescore">nb.Clicks </h1>
                 </br>
@@ -40,7 +35,7 @@
 
              </div>
 
-            
+
         </div>
 
         <div class="classement">
@@ -49,55 +44,43 @@
             <table class="tab2">
                 <tr>
                     <th>Rang</th>
-                    <th>Pseudo</th> 
-                    <th>Nb.Réussite</th> 
-                    <th>Niveau de difficulté</th> 
-                    <th>Score du joueur</th>
-
+                    <th>Pseudo</th>
+                    <th>Nb.Réussite</th>
 
                   </tr>
                   <tr>
                     <td class="rang"> #1 </td>
-                    <td class="nomplayer"> Kevin </td> 
-                    <td class="ratio"> 20/28 </td> 
-                    <td class="level"> Difficile </td> 
-                    <td class="pts"> 200 pts </td> 
-
+                    <td class="nomplayer"> Kevin </td>
+                    <td class="ratio"> 20/28 </td>
                   </tr>
                   <tr>
                     <td class="rang"> #2 </td>
                     <td class="nomplayer"> Paul </td>
                     <td class="ratio"> 18/28 </td>
-                    <td class="level"> Difficile </td> 
-                    <td class="pts"> 100 pts </td>   
                   </tr>
                   <tr>
                     <td class="rang"> #3 </td>
                     <td class="nomplayer"> Jacques </td>
                     <td class="ratio"> 17/28 </td>
-                    <td class="level"> Moyen </td> 
-                    <td class="pts"> 100 pts </td>  
                   </tr>
                   <tr>
                     <td class="rang"> #4 </td>
                     <td class="nomplayer"> Louis </td>
                     <td class="ratio"> 16/28 </td>
-                    <td class="level"> Difficile </td> 
-                    <td class="pts"> 400 pts </td>  
                   </tr>
                   <tr>
                     <td class="rang"> #5 </td>
-                    <td class="nomplayer"> Charles </td> 
+                    <td class="nomplayer"> Charles </td>
                     <td class="ratio"> 14/28 </td>
-                    <td class="level"> Facile </td> 
-                    <td class="pts"> 100 pts </td> 
                   </tr>
             </table>
-           
-        
+
+
         </div>
 
     </div>
+    <?php
+  require '../include/footer.inc.php';
+  ?>
 </body>
 </html>
-
